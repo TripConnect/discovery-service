4aa383ffbc72c2a66318d684599bb0726ebd45f5
ndtrong <nguyenductrong12a1@gmail.com>
2025-04-08 01:09:07 +0700

    Update: Strong type for rpc impl

--- src/rpc.ts
+++ src/rpc.ts
@@ -1,7 +1,11 @@
 const grpc = require('@grpc/grpc-js');
+import { sendUnaryData, ServerUnaryCall } from "@grpc/grpc-js";
 import request from "sync-request";
+import { backendDefinition } from 'common-utils';
 
-export async function discover(call: any, callback: any) {
+export async function discover(
+    call: ServerUnaryCall<backendDefinition.discovery_service.DiscoveryRequest, backendDefinition.discovery_service.ServiceInstance>,
+    callback: sendUnaryData<backendDefinition.discovery_service.ServiceInstance>) {
     try {
         let configHost = process.env.NODE_ENV === "local" ? "localhost" : "config-service";
         let { serviceName } = call.request;
@@ -12,14 +16,14 @@ export async function discover(call: any, callback: any) {
         }
         let configs = JSON.parse(resp.getBody("utf-8")).data;
 
-        callback(null, {
+        callback(null, new backendDefinition.discovery_service.ServiceInstance({
             host: process.env.NODE_ENV === "local" ? "localhost" : serviceName,
             port: configs.server.port,
-        });
+        }));
     } catch (error: any) {
         callback({
             code: grpc.status.NOT_FOUND,
-            message: 'Authorization failed'
+            message: "Service not found"
         });
     }
 }
